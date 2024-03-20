f4389baf102f25a91ab20b01db2441f05084e2aa
tosu <timo42@proton.me>
2024-03-20 04:18:23 +0100

    hashes

--- benchmark.sh
+++ benchmark.sh
@@ -9,9 +9,10 @@ fi
 
 command -v python3 1>/dev/null || { echo "Can't find python3, exiting."; exit 2; }
 command -v valgrind 1>/dev/null || { echo "Can't find valgrind, exiting."; exit 3; }
-command -v xargs 1>/dev/null || { echo "Can't find xargs, exiting."; exit 4; } # just to be sure...
-command -v bc 1>/dev/null || { echo "Can't find bc, exiting."; exit 5; } # just to be sure...
-command -v shasum 1>/dev/null || { echo "Can't find shasum, exiting."; exit 5; } # should be sha1sum by default
+command -v xargs 1>/dev/null || { echo "Can't find xargs, exiting."; exit 4; }
+command -v bc 1>/dev/null || { echo "Can't find bc, exiting."; exit 5; }
+command -v shasum 1>/dev/null || { echo "Can't find shasum, exiting."; exit 6; } # should be sha1sum by default
+command -v wget 1>/dev/null || { echo "Can't find wget, exiting."; exit 7; }
 
 generator="./npuzzle-gen.py"
 verifier="./npuzzle-verify.py"
@@ -19,8 +20,8 @@ verifier="./npuzzle-verify.py"
 sha1sum_puzzle_gen="$(shasum 2>/dev/null < "${generator}" | cut -f1 -d' ')"
 sha1sum_puzzle_verify="$(shasum 2>/dev/null < "${verifier}" | cut -f1 -d' ')"
 
-[ -f "${generator}" ] && [ "${sha1sum_puzzle_gen}" = "3fa00f017bbbb90576861b466bc7dfde06946145" ] || wget -qO "${generator}" 'https://gist.githubusercontent.com/cubernetes/39a9d35a241386f2fb7e6c4f3bdd58d6/raw/1ba7a6ea47d835f5e3f24e1eca5707ee45edb48e/npuzzle-gen.py'
-[ -f "${verifier}" ] && [ "${sha1sum_puzzle_gen}" = "927044cb9b3bb0b93c283d3a4488b6501ce7a29b" ] || wget -qO "${verifier}" 'https://gist.githubusercontent.com/cubernetes/39a9d35a241386f2fb7e6c4f3bdd58d6/raw/1ba7a6ea47d835f5e3f24e1eca5707ee45edb48e/npuzzle-verify.py'
+[ -f "${generator}" ] && [ "${sha1sum_puzzle_gen}" = "3fa00f017bbbb90576861b466bc7dfde06946145" ] || wget -qO "${generator}" 'https://gist.githubusercontent.com/cubernetes/39a9d35a241386f2fb7e6c4f3bdd58d6/raw/a5a8895caf987cb9b465c3b1685f4bd063fdd5fa/npuzzle-gen.py'
+[ -f "${verifier}" ] && [ "${sha1sum_puzzle_gen}" = "927044cb9b3bb0b93c283d3a4488b6501ce7a29b" ] || wget -qO "${verifier}" 'https://gist.githubusercontent.com/cubernetes/39a9d35a241386f2fb7e6c4f3bdd58d6/raw/a5a8895caf987cb9b465c3b1685f4bd063fdd5fa/npuzzle-verify.py'
 chmod +x "${generator}"
 chmod +x "${verifier}"
 
